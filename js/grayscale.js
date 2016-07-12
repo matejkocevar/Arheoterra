f3719eeec770149f22054af5ae5263fd0fc6dd70
Matej Kočevar <kocevar.matej@gmail.com>
2016-07-12 23:37:44 +0200

    Opisi projektov imajo galerijo, dodati je treba še pomanjšane slike v img/ Zamenjava Owl Carousel za Slick

--- js/grayscale.js
+++ js/grayscale.js
@@ -44,3 +44,52 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function(){
+    $('.slider-storitve').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+    });
+    $('.slider-slike').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: '0px',
+    });
+});
