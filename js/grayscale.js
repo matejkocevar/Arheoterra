fffb62e110e70744960e93081aabc30b6e6282af
Matej Kočevar <kocevar.matej@gmail.com>
2016-07-14 22:18:39 +0200

    Narejene povezave

--- js/grayscale.js
+++ js/grayscale.js
@@ -46,40 +46,15 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(){
-    $('.slider-storitve').slick({
+    $('.slider-povezave').slick({
         dots: true,
+        arrows: true,
         infinite: true,
         speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
         autoplay: false,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-
+        autoplaySpeed: 2000
     });
     $('.slider-slike').slick({
         dots: true,
@@ -90,6 +65,6 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         centerMode: true,
-        centerPadding: '0px',
+        centerPadding: '0px'
     });
 });
